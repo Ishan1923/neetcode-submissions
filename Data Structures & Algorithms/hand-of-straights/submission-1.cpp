f3ce9132561c2ec747b98eae30c64fe5bc7b4419
Ishan Pathak <148351455+Ishan1923@users.noreply.github.com>
2026-08-26 05:13:58 +0530

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size() % groupSize != 0) return false;
+        map<int, int> freq;
+        for(int num : hand){
+            freq[num]++;
+        }
+        
+        for(auto [n, f] : freq){
+            if(f == 0) continue;
+            for(int i = 0; i < groupSize; i++){
+
+                int nxt_card = n + i;
+                if(freq[nxt_card] < f){
+                    return false;
+                }
+                freq[nxt_card]-=f;
+
+            }
+        }
+
+        return true;
+    }
+};
