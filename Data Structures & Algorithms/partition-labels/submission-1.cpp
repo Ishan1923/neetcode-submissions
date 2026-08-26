b7ecec5b8a8932e6a12562047126b13873795aa1
Ishan Pathak <148351455+Ishan1923@users.noreply.github.com>
2026-08-27 00:36:51 +0530

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.cpp
@@ -0,0 +1,95 @@
+class Solution {
+public:
+    // vector<int> partitionLabels(string s) {
+    //     int n = s.size();
+    //     unordered_map<char, pair<int, int>> intervals;
+    //     for(int i = 0; i < n; i++){
+            
+    //         if(intervals.find(s[i]) == intervals.end()){
+    //             intervals[s[i]] = {i, i};
+    //         }
+    //         else{
+    //             intervals[s[i]].first = min(intervals[s[i]].first, i);
+    //             intervals[s[i]].second = max(intervals[s[i]].second, i);
+    //         }
+
+    //     }
+
+
+    //     vector<vector<int>> intrs;
+
+
+    //     for(auto& [c, interval] : intervals){
+    //         intrs.push_back({interval.first, interval.second});
+    //     }
+
+
+    //     sort(intrs.begin(), intrs.end());
+
+
+    //     vector<pair<int, int>> merged;
+    //     for(int i = 0; i < intrs.size(); i++){
+
+    //         auto interval = intrs[i];
+
+    //         if(!merged.empty() && interval[0] <= merged.back().second){
+    //             merged.back().second = max(merged.back().second, interval[1]);
+    //         }
+    //         else{
+    //             merged.push_back({interval[0], interval[1]});
+    //         }
+
+
+    //     }
+
+
+
+    //     vector<int> result;
+
+    //     for(int i = 0 ; i < merged.size(); i++){
+    //         result.push_back(merged[i].second - merged[i].first + 1);
+    //     }
+
+
+    //     return result;
+
+
+
+    // }
+
+
+
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        int last_idx[26] = {0};
+
+        for(int i = 0; i < n; i++){
+            last_idx[s[i] - 'a'] = i;
+        }
+
+        for(int i = 0; i < n; i++){
+            last_idx[s[i] - 'a'] = max(i, last_idx[s[i] - 'a']);
+        }
+
+
+        vector<int> res;
+
+        int end = -1;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            end = max(end, last_idx[s[i] - 'a']);
+
+            count++;
+
+            if(i == end){
+                res.push_back(count);
+                count = 0;
+                end = -1;
+            }
+
+        }
+
+        return res;
+
+    }
+};
