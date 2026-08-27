b7af0236a774b91dcefc9834e23fc2ac6b37ce0a
Ishan Pathak <148351455+Ishan1923@users.noreply.github.com>
2026-08-27 22:57:20 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+
+    void f(int ind, vector<int>& subset, const vector<int>& nums, vector<vector<int>>& subsets){
+
+        subsets.push_back(subset);
+
+        if(ind >= nums.size()){
+            return;
+        }
+
+        for(int i = ind; i < nums.size(); i++){
+            if(i != ind && nums[i] == nums[i - 1]) continue;
+            
+            subset.push_back(nums[i]);
+            f(i + 1, subset, nums, subsets);
+            subset.pop_back();
+        }
+        
+
+
+    }
+
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> subset;
+        vector<vector<int>> subsets;
+        f(0, subset, nums, subsets);
+        return subsets;
+    }
+};
